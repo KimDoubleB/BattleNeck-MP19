f596171fe4cfb99da5e6a2d137007a03001ee421
KimDoubleB <tree9295@gamil.com>
2019-05-26 12:07:37 +0900

    Add tab view (data & rank)

---
+++ app/src/main/java/com/example/term19/Main2Activity.java
@@ -0,0 +1,31 @@
+package com.example.term19;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+        TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1) ;
+        tabHost1.setup();
+
+        // 첫 번째 Tab. (탭 표시 텍스트:"TAB 1"), (페이지 뷰:"content1")
+        TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1") ;
+        ts1.setContent(R.id.content1) ;
+        ts1.setIndicator("Data") ;
+        tabHost1.addTab(ts1)  ;
+
+        // 두 번째 Tab. (탭 표시 텍스트:"TAB 2"), (페이지 뷰:"content2")
+        TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2") ;
+        ts2.setContent(R.id.content2) ;
+        ts2.setIndicator("Rank") ;
+        tabHost1.addTab(ts2) ;
+
+    }
+}
--- app/src/main/java/com/example/term19/MainHomeActivity.java
+++ app/src/main/java/com/example/term19/MainHomeActivity.java
@@ -6,17 +6,28 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.TabHost;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class MainHomeActivity extends AppCompatActivity {
     private TextView tv_roll;
-    private Button login;
-    private Button rank;
+
+    public TextView rank;
 
     /*Used for Accelometer & Gyroscoper*/
     private SensorManager mSensorManager = null;
@@ -45,14 +56,31 @@ public class MainHomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_home);
 
-        tv_roll = findViewById(R.id.tv_roll);
-        login = findViewById(R.id.btnLogin);
+        // Tab 2
         rank = findViewById(R.id.ranking);
+        setRanking();
 
+        // Tab 1
+        tv_roll = findViewById(R.id.tv_roll);
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         userSensorListner = new UserSensorListner();
         mGyroscopeSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1) ;
+        tabHost1.setup();
+
+        // 첫 번째 Tab. (탭 표시 텍스트:"TAB 1"), (페이지 뷰:"content1")
+        TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1") ;
+        ts1.setContent(R.id.content1) ;
+        ts1.setIndicator("Data") ;
+        tabHost1.addTab(ts1)  ;
+
+        // 두 번째 Tab. (탭 표시 텍스트:"TAB 2"), (페이지 뷰:"content2")
+        TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2") ;
+        ts2.setContent(R.id.content2) ;
+        ts2.setIndicator("Rank") ;
+        tabHost1.addTab(ts2) ;
 
         // event listener
         findViewById(R.id.filter).setOnClickListener(new View.OnClickListener() {
@@ -74,16 +102,48 @@ public class MainHomeActivity extends AppCompatActivity {
             }
         });
 
-        rank.setOnClickListener(new View.OnClickListener(){
+    }
+
+    // Tab view 2 function
+    public void setRanking() {
+        // Get a reference to our posts
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("id_list/");
+
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onClick(View view){
-                Intent intent = new Intent(getApplicationContext(), RankingActivity.class);
-                startActivity(intent);
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // All data in Firebase DB
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    HashMap<String, Object> data = (HashMap<String, Object>) snapshot.getValue();
+                    /*
+                    //    this.id = id;
+                        this.pw = pw;
+                        this.age = age;
+                        this.gender = gender;
+                    //    this.score = score;
+                    */
+
+                    String dataID = ((HashMap<String, Object>) snapshot.getValue()).get("id").toString();
+                    String dataScore = ((HashMap<String, Object>) snapshot.getValue()).get("score").toString();
+                    Log.d("Message", dataID + " " + dataScore);
+
+                    rank.setText(rank.getText() + dataID + " : " + dataScore + '\n');
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
             }
         });
+
     }
 
 
+
+    // Tab view 1 function
     /**
      * 1차 상보필터 적용 메서드
      */
@@ -161,4 +221,10 @@ public class MainHomeActivity extends AppCompatActivity {
         public void onAccuracyChanged(Sensor sensor, int accuracy) {
         }
     }
+
+
+    public void onBackPressed() {
+        finish();
+    }
+
 }
